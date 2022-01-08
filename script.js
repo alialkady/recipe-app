210e70e6e4e0259080d4165ae9d2d209a8a5666e
alialkady <66703864+alialkady@users.noreply.github.com>
2022-01-08 12:49:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+getRandomMeal();
+async function getRandomMeal(){
+const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+const respData = await resp.json();
+const randomMeal = respData.meals[0];
+console.log(randomMeal);
+}
+async function getMealById(id){
+const meal =   await fetch("htttps://www.themealdb.com/api/json/v1/1/lookup.php?i=52772"+id);
+
+};
+async function getMealBySearch(term){
+const meals = await fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata") +term;
+};
